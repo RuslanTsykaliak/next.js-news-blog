c14ba1d19e785a2d48fdc1e12bb75b56400be09a
Mac <mac@MacBook-Pro-2.local>
2024-01-01 14:56:31 +0200

    update 8

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,4 +1,66 @@
-import { AuthOptions } from "next-auth";
+//Origina code
+
+// import { AuthOptions } from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+
+// import prisma from "@/lib/prismadb";
+
+// const authOptions: AuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID as string,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/sign-in",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+
+// export default authOptions;
+
+
+// Options
+
+// 1 // pnpm i @prisma/client @auth/prisma-adapter
+
+// import NextAuth from "next-auth/next"; // Adjusted import
+// import GitHubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
+// import { PrismaAdapter } from "next-auth/adapters/prisma"; // Corrected import path
+
+// import prisma from "@/lib/prismadb";
+
+// const authOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID as string,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/sign-in",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+
+
+// 2
+
+import NextAuth, { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -7,7 +69,7 @@ import prisma from "@/lib/prismadb";
 
 const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
-  providers: [
+  providers:  [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
@@ -24,3 +86,4 @@ const authOptions: AuthOptions = {
 };
 
 export default authOptions;
+
