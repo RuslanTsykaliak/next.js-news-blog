7711c248e31fbe1b74060470a627b1b7a834ee3d
Mac <mac@MacBook-Pro-2.local>
2024-01-01 14:24:55 +0200

    update 6

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,47 @@
-// Type error: Route "app/api/auth/[...nextauth]/route.ts" does not match the required types of a Next.js Route. 
-// "authOptions" is not a valid Route export field.
+// 5 ssplit the code into two files: authOptions.ts and route.ts.
+// Fixed: Type error: Route "app/api/auth/[...nextauth]/route.ts" does not match the required types of a Next.js Route.
+  // "authOptions" is not a valid Route export field.
 
-import NextAuth, { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
-import prisma from "@/lib/prismadb";
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: "/sign-in",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import authOptions from "./authOptions";
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+
+// Original code
+// Type error: Route "app/api/auth/[...nextauth]/route.ts" does not match the required types of a Next.js Route. 
+// "authOptions" is not a valid Route export field.
+
+// import NextAuth, { AuthOptions } from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+
+// import prisma from "@/lib/prismadb";
+
+// export const authOptions: AuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID as string,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/sign-in",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
 
 // Options:
 
@@ -99,3 +112,19 @@ export { handler as GET, handler as POST };
 // export { handler as GET, handler as POST };
 
 // Type error: Argument of type '{ adapter: Adapter;
+
+
+
+
+// 3
+// remove:
+// export
+// adapter: PrismaAdapter(prisma),
+
+
+
+// 4
+// remove .ts from route.ts file.
+// pnpm i --save-dev typescript ts-loader
+
+
